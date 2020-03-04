ad16c59feb06f6399fba4144b345bc02ab9361cc
Sergej <sergej@computerassistance.co.uk>
2020-03-04 14:40:31 +0000

    mult dim arr

---
+++ PhpBeginner/Php_Book Arrays/02-MulriDimensional Arrays/example01.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$customer_record[0][0] = "Pete";
+$customer_record[0][1] = "Smith";
+$customer_record[0][2] = "123 Main";
+$customer_record[0][3] = "Atlanta";
+$customer_record[0][4] = "GA";
+$customer_record[0][5] = 30001;
+$customer_record[1][0] = "Sally";
+$customer_record[1][1] = "Parisi";
+$customer_record[1][2] = "101 South";
+$customer_record[1][3] = "Atlanta";
+$customer_record[1][4] = "GA";
+$customer_record[1][5] = 30001;
+
+
+?>
+
