3bab4ed9c349d729d590f05ab73af25fe58970c8
Sergej <sergej@computerassistance.co.uk>
2020-03-04 16:08:36 +0000

    sqlite

--- PhpBeginner/Php_Book Arrays/02-MulriDimensional Arrays/example03.php
+++ PhpBeginner/Php_Book Arrays/02-MulriDimensional Arrays/example03.php
@@ -2,24 +2,45 @@
 
 // Add to Database
 
-echo (" Multi Arrays From Json file: <br>");
-$data_file_loc = ("customer_data.json");
-$customer_file = file_get_contents($data_file_loc);
-$customer_record = json_decode($customer_file, TRUE);
+echo (" Multi Arrays From SQLITE: <br>");
+// $ver = SQLite3::version();
 
-$customer = filter_input_array( INPUT_POST );
-$customer_info = $customer['customer_record'];
+// Only on POST
+if ($_POST ) {
 
-array_push($customer_record, $customer_info);
+    $customer = filter_input_array( INPUT_POST );
+    $customer_info = $customer['customer_record'];
 
-print_r($customer_record);
+    // print_r($ver);
+    if (
+        // $db = sqlite_open('test', 0666, $sqliteerror)
+        $db = new SQLite3('test.sqlite')
+        ) { 
+        print_r('In database<br>');
+        $db->exec("CREATE TABLE friends(id INTEGER PRIMARY KEY, firstName TEXT, lastName TEXT, address1 TEXT, city TEXT, zipcode Text)");
+            $db->exec("INSERT INTO friends(firstName , lastName , address1 , city , zipcode) 
+                VALUES ('Hello','Hello','Hello','Hello','Hello')
+            ");
+        
+        $res = $db->query('SELECT * FROM friends');
+        while ($res = $res->fetchArray() ) {
+            print_r($res); 
+        }
+    } else {
+        print_r('Error');
+        die($sqliteerror);
+    }
 
-file_put_contents($data_file_loc, json_encode($customer_record) );
+    // print_r($customer_record);
+
+}
+
+
 
 
 
 echo "<pre>";
-print_r( $customer_record );
+// print_r( $customer_record );
 echo "</pre>";
 
 
