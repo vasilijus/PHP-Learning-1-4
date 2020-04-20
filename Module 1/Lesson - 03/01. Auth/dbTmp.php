7a086a9df6f453b3e2f89d88b9c77308e6c1c361
vasilijus <vasiliokas@gmail.com>
2020-04-21 00:03:11 +0100

    tmp db change

---
+++ Module 1/Lesson - 03/01. Auth/dbTmp.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$users = [
+
+    [
+        'email'     => 'test@mail.com',
+        'login'     => 'test1',
+        'password'  => 'qwerty'
+    ],
+    [
+        'email'     => 'admin@mail.com',
+        'login'     => 'admin',
+        'password'  => 'qwerty'
+    ]
+
+];
+
+?>
--- Module 1/Lesson - 03/01. Auth/index.php
+++ Module 1/Lesson - 03/01. Auth/index.php
@@ -6,4 +6,4 @@ include_once 'config.php';
 
 include_once 'functions.php';
 
-include_once 'dbconn.php';
+include_once 'dbTmp.php';
