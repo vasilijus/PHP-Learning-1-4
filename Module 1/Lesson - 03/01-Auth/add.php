f2284fd5aa975dd76998495b8ca8a67db7ff36aa
vasilijus <vasiliokas@gmail.com>
2020-04-29 00:12:00 +0100

    ISSUE: Call to undefined function checkRegularExp()

--- Module 1/Lesson - 03/01-Auth/add.php
+++ Module 1/Lesson - 03/01-Auth/add.php
@@ -41,4 +41,27 @@ if(isset($_POST['addArticle'])) {
 else {
     $message = '';
 }
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Add article page</title>
+</head>
+<body>
+    
+    <form method="post">
+        Название<br>
+        <input type="text" name="title" value="<?php echo $title?>"> <br>
+        Контент<br>
+        <textarea name="content"><?php echo $content?></textarea><br>
+        <input type="submit" value="Добавить" name="addArticle">
+    </form>
+    <?php echo $message; ?>
+
+</body>
+</html>
