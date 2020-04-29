b245e38edb2eca13f786f25ebe28be6b20a5ba7c
vasilijus <vasiliokas@gmail.com>
2020-04-29 23:24:53 +0100

    adding a dir on index.php checck

--- Module 1/Lesson - 03/01-Auth/index.php
+++ Module 1/Lesson - 03/01-Auth/index.php
@@ -25,4 +25,13 @@ if( file_exists("$dataDir") ) {
     $dirExists = true;
     // list files
     $listDir = scandir("$dataDir");
+
+    foreach( $listDir as $article ){
+
+        $articles[] = "<li><a href=\"post.php?fname=$fname\">P. $fname</a><a href=\"edit.php?fname=$fname\">E. $fname</a></li>";
+    }
+}
+else {
+    $dirExists = false;
+    mkdir($dataDir, 0775);
 }
